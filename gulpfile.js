e08d700b130eae92147e6da31b7218868c41f430
Ryan LaBouve <imryanlabouve@gmail.com>
2015-09-20 00:31:24 -0500

    Switch to browsersync. max speed. much excite

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,63 @@
 'use strict';
-
 var gulp = require('gulp');
 var run = require('gulp-run');
-var lr = require('gulp-livereload');
-var webserver = require('gulp-webserver');
+// var webserver = require('gulp-webserver');
 var chalk = require('chalk');
 
 // CSS Junk
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+});
+
 // Metalsmith
 gulp.task('metalsmith', function() {
   // this runs metalsmith
-  run('npm start').exec();
-  console.log(chalk.blue('Sweet Jesus, there\'s a fire!'));
+  return run('npm start').exec('', function() {
+    return gulp.start('css');
+  });
 });
 
 // SCSS
 gulp.task('css', function() {
   var processors = [
-    autoprefixer({browsers: ['last 1 version']})
+    autoprefixer({browsers: ['last 1 version']}),
+    require('postcss-nested'),
+    require('postcss-simple-vars'),
+    require('postcss-mixins')
   ];
 
   return gulp.src('css/**/*.css')
           .pipe(postcss(processors))
-          .pipe(gulp.dest('./dist/css'));
+          .pipe(gulp.dest('./dist/css'))
+          .pipe(browserSync.stream());
 });
 
 
 // Post
 gulp.task('watch', function() {
-  lr.listen();
-  gulp.watch('content/**/*', ['metalsmith']);
-  gulp.watch('layouts/**/*', ['metalsmith']);
+  gulp.watch('content/**/*', ['metalsmith', 'css']);
+  gulp.watch('layouts/**/*', ['metalsmith', 'css']);
   gulp.watch('css/**/*', ['css']);
+  gulp.watch("dist/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('webserver', function() {
-  gulp.src('dist')
-    .pipe(webserver({
-      livereload: true,
-      port: 9191,
-      open: true
-    }));
-});
+// gulp.task('webserver', function() {
+//   gulp.src('dist')
+//     .pipe(webserver({
+//       livereload: true,
+//       port: 9191,
+//       open: true
+//     }));
+// });
 
-gulp.task('default', ['metalsmith', 'watch', 'css', 'webserver'])
+gulp.task('default', ['serve', 'watch','metalsmith', 'css'])
